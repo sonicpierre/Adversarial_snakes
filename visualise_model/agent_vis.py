f59af29a516188b1483c6aceac993c8e1b19b12c
sonicpierre <pvirgaux@gmail.com>
2022-06-06 12:17:44 +0200

    Visualize multiple snakes

--- visualise_model/agent_vis.py
+++ visualise_model/agent_vis.py
@@ -11,45 +11,46 @@ LR = 0.001
 
 class Agent:
 
-    def __init__(self, model_path):
+    def __init__(self, model_path, snake):
         self.n_games = 0
         self.epsilon = 0 # randomness
         self.gamma = 0.9 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
         self.model = Linear_QNet(11, 512, 3)
         self.model.load_state_dict(torch.load(model_path))
-
+        self.snake = snake
 
     def get_state(self, game):
-        head = game.snake[0]
+
+        head = self.snake.snake[0]
         point_l = Point(head.x - 20, head.y)
         point_r = Point(head.x + 20, head.y)
         point_u = Point(head.x, head.y - 20)
         point_d = Point(head.x, head.y + 20)
         
-        dir_l = game.direction == Direction.LEFT
-        dir_r = game.direction == Direction.RIGHT
-        dir_u = game.direction == Direction.UP
-        dir_d = game.direction == Direction.DOWN
+        dir_l = self.snake.direction == Direction.LEFT
+        dir_r = self.snake.direction == Direction.RIGHT
+        dir_u = self.snake.direction == Direction.UP
+        dir_d = self.snake.direction == Direction.DOWN
 
         state = [
             # Danger straight
-            (dir_r and game.is_collision(point_r)) or 
-            (dir_l and game.is_collision(point_l)) or 
-            (dir_u and game.is_collision(point_u)) or 
-            (dir_d and game.is_collision(point_d)),
+            (dir_r and game.is_collision(self.snake, pt = point_r)) or 
+            (dir_l and game.is_collision(self.snake, pt = point_l)) or 
+            (dir_u and game.is_collision(self.snake, pt = point_u)) or 
+            (dir_d and game.is_collision(self.snake, pt = point_d)),
 
             # Danger right
-            (dir_u and game.is_collision(point_r)) or 
-            (dir_d and game.is_collision(point_l)) or 
-            (dir_l and game.is_collision(point_u)) or 
-            (dir_r and game.is_collision(point_d)),
+            (dir_u and game.is_collision(self.snake, pt = point_r)) or 
+            (dir_d and game.is_collision(self.snake, pt = point_l)) or 
+            (dir_l and game.is_collision(self.snake, pt = point_u)) or 
+            (dir_r and game.is_collision(self.snake, pt = point_d)),
 
             # Danger left
-            (dir_d and game.is_collision(point_r)) or 
-            (dir_u and game.is_collision(point_l)) or 
-            (dir_r and game.is_collision(point_u)) or 
-            (dir_l and game.is_collision(point_d)),
+            (dir_d and game.is_collision(self.snake, pt = point_r)) or 
+            (dir_u and game.is_collision(self.snake, pt = point_l)) or 
+            (dir_r and game.is_collision(self.snake, pt = point_u)) or 
+            (dir_l and game.is_collision(self.snake, pt = point_d)),
             
             # Move direction
             dir_l,
@@ -58,26 +59,39 @@ class Agent:
             dir_d,
             
             # Food location 
-            game.food.x < game.head.x,  # food left
-            game.food.x > game.head.x,  # food right
-            game.food.y < game.head.y,  # food up
-            game.food.y > game.head.y  # food down
+            game.food.x < self.snake.head.x,  # food left
+            game.food.x > self.snake.head.x,  # food right
+            game.food.y < self.snake.head.y,  # food up
+            game.food.y > self.snake.head.y  # food down
             ]
 
         return np.array(state, dtype=int)
 
 
 def visualise():
-    game = SnakeGameAI()
-    models = os.listdir('model')
-    models = sorted(models, key=lambda model: int(model.split()[1].replace(".pth", "")))
 
-    for model_path in models:
+    nb_snake = 1
+    game = SnakeGameAI(nb_snake)
+    models_dir = os.listdir('model')
+    models_dir = sorted(models_dir, key=lambda model: int(model))
+    models = []
+    for dir in models_dir:
+        dir_path = os.path.join('model', dir)
+        path = os.listdir(dir_path)
+        models.append(os.path.join(dir_path,np.random.choice(path)))
 
-        agent = Agent(os.path.join('model', model_path))
-        while True:
-            
-            game.model_display_name = model_path.replace('.pth', '')
+    agents = []
+    for snake, model_path in zip(game.snakes, models):
+        agents.append(Agent(model_path, snake))
+
+
+    while True:
+
+        actions = []
+        for agent in agents:
+            # get old state
+            state_old = agent.get_state(game)
+        
             # get old state
             state_old = torch.tensor(agent.get_state(game)).type(torch.FloatTensor)
 
@@ -86,11 +100,11 @@ def visualise():
 
             final_move = np.zeros(3, dtype=int)
             final_move[torch.argmax(final_pred)] = 1
+            actions.append(final_move)
 
-            # perform move and get new state
-            _, done, _ = game.play_step(final_move)
+        # perform move and get new state
+        done = game.play_step(actions)
 
-            if done:
-                # train long memory, plot result
-                game.reset()
-                break
+        if done:
+            game.reset()
+            break
--- visualise_model/game_vis.py
+++ visualise_model/game_vis.py
@@ -20,6 +20,9 @@ class Direction(Enum):
 
 Point = namedtuple('Point', 'x, y')
 
+W=640
+H=480
+
 # rgb colors
 WHITE = (255, 255, 255)
 RED = (200,0,0)
@@ -30,117 +33,30 @@ BLACK = (0,0,0)
 BLOCK_SIZE = 20
 SPEED = 40
 
-class SnakeGameAI:
+class Snake:
 
-    def __init__(self, w=640, h=480):
-        self.w = w
-        self.h = h
-        self.model_display_name = ""
-        # init display
-        self.display = pygame.display.set_mode((self.w, self.h))
-        pygame.display.set_caption('Snake')
-        self.clock = pygame.time.Clock()
-        self.reset()
-
-
-    def reset(self):
-        # init game state
-        self.direction = Direction.RIGHT
-
-        self.head = Point(self.w/2, self.h/2)
+    def __init__(self, name:int):
+        self.name = name
+        self.reward = 0
+        self.head = Point((np.random.randint(0,W)%20)*20, (np.random.randint(0,H)%20)*20)
         self.snake = [self.head,
                       Point(self.head.x-BLOCK_SIZE, self.head.y),
                       Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
-
         self.score = 0
-        self.food = None
-        self._place_food()
-        self.frame_iteration = 0
+        self.direction = Direction.RIGHT
 
-
-    def _place_food(self):
-        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
-        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
-        self.food = Point(x, y)
-        if self.food in self.snake:
-            self._place_food()
-
-
-    def play_step(self, action):
-        self.frame_iteration += 1
-        # 1. collect user input
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+    
+    def move(self, action):
         
-        # 2. move
-        self._move(action) # update the head
-        self.snake.insert(0, self.head)
-        
-        # 3. check if game over
-        reward = 0
-        game_over = False
-        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
-            game_over = True
-            reward = -10
-            return reward, game_over, self.score
-
-        # 4. place new food or just move
-        if self.head == self.food:
-            self.score += 1
-            reward = 10
-            self._place_food()
-        else:
-            self.snake.pop()
-        
-        # 5. update ui and clock
-        self._update_ui()
-        self.clock.tick(SPEED)
-        # 6. return game over and score
-        return reward, game_over, self.score
-
-
-    def is_collision(self, pt=None):
-        if pt is None:
-            pt = self.head
-        # hits boundary
-        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
-            return True
-        # hits itself
-        if pt in self.snake[1:]:
-            return True
-
-        return False
-
-
-    def _update_ui(self):
-        self.display.fill(BLACK)
-
-        for pt in self.snake:
-            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
-
-        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
-
-        name = font.render(self.model_display_name, True, WHITE)
-        self.display.blit(name, [0, 0])
-        pygame.display.flip()
-
-
-    def _move(self, action):
         # [straight, right, left]
-
         clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
         idx = clock_wise.index(self.direction)
 
         if np.array_equal(action, [1, 0, 0]):
             new_dir = clock_wise[idx] # no change
-
         elif np.array_equal(action, [0, 1, 0]):
             next_idx = (idx + 1) % 4
             new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
-
         else: # [0, 0, 1]
             next_idx = (idx - 1) % 4
             new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
@@ -159,3 +75,115 @@ class SnakeGameAI:
             y -= BLOCK_SIZE
 
         self.head = Point(x, y)
+
+
+class SnakeGameAI:
+
+    def __init__(self, nb_snake = 1):
+        self.w = W
+        self.h = H
+        self.snakes = []
+        self.nb_snake = nb_snake
+        self.model_display_name = ""
+        # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+        self.reset()
+
+    def _create_snakes(self):
+
+        for i in range(self.nb_snake):
+            self.snakes.append(Snake(i))
+
+    def reset(self):
+        # init game state
+        self.snakes = []
+        self._create_snakes()
+
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0
+
+    def _place_food(self):
+        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        self.food = Point(x, y)
+        for snake in self.snakes:
+            if self.food in snake.snake:
+                self._place_food()
+
+
+    def play_step(self, actions):
+
+        self.frame_iteration += 1
+        # 1. collect user input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            
+
+        for snake, action in zip(self.snakes, actions):
+
+            # 2. move
+            snake.move(action) # update the head
+            snake.snake.insert(0, snake.head)
+            
+            # 3. check if game over
+            snake.reward = 0
+            game_over = False
+
+            if self.is_collision(snake) or self.frame_iteration > 100*len(snake.snake):
+                game_over = True
+                snake.reward = -10
+                return game_over
+
+            # 4. place new food or just move
+            if snake.head == self.food:
+                snake.reward = 10
+                snake.score += 1
+                self._place_food()
+            else:
+                snake.snake.pop()
+        
+        # 5. update ui and clock
+        self._update_ui()
+        self.clock.tick(SPEED)
+        # 6. return game over and score
+        return game_over
+
+
+    def is_collision(self, my_snake, pt=None):
+        if pt is None:
+            pt = my_snake.head
+
+        # hits boundary
+        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
+            return True
+
+        # hits itself
+        if pt in my_snake.snake[1:]:
+            return True
+
+        # hits other snakes
+        for snake in self.snakes:
+            if(snake.name != my_snake.name):
+                if pt in snake.snake:
+                    return True
+
+        return False
+
+
+    def _update_ui(self):
+        self.display.fill(BLACK)
+        for snake in self.snakes:
+            for pt in snake.snake:
+                pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+                pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+
+            pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+
+        name = font.render(self.model_display_name, True, WHITE)
+        self.display.blit(name, [0, 0])
+        pygame.display.flip()
